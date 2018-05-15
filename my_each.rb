b1def815eab76a0d15f755946d594a6ab73e711e
Alex Wilson <fonzmp@gmail.com>
2018-05-15 15:58:18 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,12 @@
 def my_each(array) # put argument(s) here
   # code here
-  i = 0
-  while i < array.length
-    yield
-    i += 1
-  end
+  if block_given?
+
+    i = 0
+    while i < array.length
+      yield
+      i += 1
+    end
+  else
+    puts "Hey, you didn't put a block in!"
 end
